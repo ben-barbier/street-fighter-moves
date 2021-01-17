aa3c6ca2d11c200775702f69a1e6793709932b5a
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-01-17 12:35:53 +0100

    🔍 : add country page tags

--- src/app/pages/country-page/country-page.component.ts
+++ src/app/pages/country-page/country-page.component.ts
@@ -15,14 +15,27 @@ export class CountryPageComponent implements OnDestroy {
     public characters: Character[] = [];
     public maxStamina = maxStamina(1);
     public maxStun = maxStun(1);
+    public country = '';
     public otherCountries: string[] = [];
-
-    // TODO: manage title and tags
 
     constructor(private route: ActivatedRoute, private title: Title, private meta: Meta) {
         this.route.data.pipe(map(resolveData => resolveData.character)).subscribe(characters => {
+            this.country = characters[0].country;
             this.characters = characters;
             this.otherCountries = [...new Set(countries(1).filter(c => c !== this.characters[0].country))];
+
+            const titleText = `Street Fighter Moves - ${this.country}`;
+            title.setTitle(titleText);
+
+            const description = `Street Fighter 4 Arcade Edition - ${this.country} characters`;
+            meta.updateTag({ name: 'description', content: description });
+
+            // 📖 : https://developers.facebook.com/docs/sharing/webmasters#markup
+            meta.updateTag({ name: 'og:url', content: document.URL });
+            meta.updateTag({ name: 'og:type', content: 'website' });
+            meta.updateTag({ name: 'og:title', content: titleText });
+            meta.updateTag({ name: 'og:description', content: description });
+            meta.updateTag({ name: 'og:image', content: `${document.baseURI}assets/countries/${this.country}.svg` });
         });
     }
 
@@ -31,6 +44,6 @@ export class CountryPageComponent implements OnDestroy {
     }
 
     private removeMetaTags(): void {
-        [].forEach(tag => this.meta.removeTag(`name='${tag}'`));
+        ['description', 'og:url', 'og:type', 'og:title', 'og:description', 'og:image'].forEach(tag => this.meta.removeTag(`name='${tag}'`));
     }
 }
