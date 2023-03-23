67b00887e0c9017258920e4fba22f5410a5f2a9a
Benjamin Barbier <benjamin@tech-homies.io>
2023-03-23 18:57:41 +0100

    refactor: improve resolvers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterPageComponent } from './pages/character-page/character-page.component';
 import { CharacterPageGuard } from './pages/character-page/character-page.guard';
-import { CharacterPageResolver } from './pages/character-page/character-page.resolver';
+import { characterPageResolver } from './pages/character-page/character-page.resolver';
 import { CharactersListPageComponent } from './pages/characters-list-page/characters-list-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
 import { CountryPageGuard } from './pages/country-page/country-page.guard';
-import { CountryPageResolver } from './pages/country-page/country-page.resolver';
+import { countryPageResolver } from './pages/country-page/country-page.resolver';
 
 const routes: Routes = [
   {
@@ -17,13 +17,13 @@ const routes: Routes = [
     path: 'sf4/characters/:characterId',
     component: CharacterPageComponent,
     canActivate: [CharacterPageGuard],
-    resolve: { character: CharacterPageResolver },
+    resolve: { character: characterPageResolver },
   },
   {
     path: 'sf4/countries/:country',
     component: CountryPageComponent,
     canActivate: [CountryPageGuard],
-    resolve: { character: CountryPageResolver },
+    resolve: { character: countryPageResolver },
   },
   { path: '**', redirectTo: '/sf4' },
 ];
--- src/app/pages/country-page/country-page.resolver.ts
+++ src/app/pages/country-page/country-page.resolver.ts
@@ -1,13 +1,10 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Character, data } from '../../data';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class CountryPageResolver implements Resolve<Character[]> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Character[]> {
-    return of(data[0].characters.filter(c => c.country === route.params['country']));
-  }
-}
+export const countryPageResolver: ResolveFn<Character[]> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+): Observable<Character[]> => {
+  return of(data[0].characters.filter(c => c.country === route.params['country']));
+};
