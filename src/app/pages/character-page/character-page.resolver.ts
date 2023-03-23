3c14b821950db1efb4d9b499694c409632da235d
Benjamin Barbier <benjamin@tech-homies.io>
2023-03-23 18:57:41 +0100

    ♻️ : improve resolvers

--- src/app/pages/character-page/character-page.resolver.ts
+++ src/app/pages/character-page/character-page.resolver.ts
@@ -1,13 +1,10 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Character, data } from '../../data';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class CharacterPageResolver implements Resolve<Character> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Character> {
-    return of(data[0].characters.find(c => c.id === route.params['characterId']) as Character);
-  }
-}
+export const characterPageResolver: ResolveFn<Character> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+): Observable<Character> => {
+  return of(data[0].characters.find(c => c.id === route.params['characterId']) as Character);
+};
