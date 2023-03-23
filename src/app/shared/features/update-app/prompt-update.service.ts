5ab35182fbe97e679b717126b69ebc74767c8c34
Benjamin Barbier <benjamin@tech-homies.io>
2023-03-23 22:48:47 +0100

    ♻️ : improve sw update check

--- src/app/shared/features/update-app/prompt-update.service.ts
+++ src/app/shared/features/update-app/prompt-update.service.ts
@@ -16,9 +16,21 @@ export class PromptUpdateService {
 
   constructor() {
     if (this.updates.isEnabled) {
-      updates.available.subscribe(() => {
-        this.appHasUpdateAvailable$.next(true);
-        this.promptUpdateDialog();
+      this.updates.versionUpdates.subscribe(evt => {
+        switch (evt.type) {
+          case 'VERSION_DETECTED':
+            console.log(`Downloading new app version: ${evt.version.hash}`);
+            break;
+          case 'VERSION_READY':
+            console.log(`Current app version: ${evt.currentVersion.hash}`);
+            console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+            this.appHasUpdateAvailable$.next(true);
+            this.promptUpdateDialog();
+            break;
+          case 'VERSION_INSTALLATION_FAILED':
+            console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+            break;
+        }
       });
     }
   }
