16a1d5fc78ee2bd10c78e1ffd7462d361d0cbbe2
Benjamin Barbier <benjamin@tech-homies.io>
2023-03-23 19:16:42 +0100

    refactor: improve guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterPageComponent } from './pages/character-page/character-page.component';
-import { CharacterPageGuard } from './pages/character-page/character-page.guard';
+import { canActivateCharacter } from './pages/character-page/character-page.guard';
 import { characterPageResolver } from './pages/character-page/character-page.resolver';
 import { CharactersListPageComponent } from './pages/characters-list-page/characters-list-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
-import { CountryPageGuard } from './pages/country-page/country-page.guard';
+import { canActivateCountry } from './pages/country-page/country-page.guard';
 import { countryPageResolver } from './pages/country-page/country-page.resolver';
 
 const routes: Routes = [
@@ -16,13 +16,13 @@ const routes: Routes = [
   {
     path: 'sf4/characters/:characterId',
     component: CharacterPageComponent,
-    canActivate: [CharacterPageGuard],
+    canActivate: [canActivateCharacter],
     resolve: { character: characterPageResolver },
   },
   {
     path: 'sf4/countries/:country',
     component: CountryPageComponent,
-    canActivate: [CountryPageGuard],
+    canActivate: [canActivateCountry],
     resolve: { character: countryPageResolver },
   },
   { path: '**', redirectTo: '/sf4' },
--- src/app/pages/character-page/character-page.guard.ts
+++ src/app/pages/character-page/character-page.guard.ts
@@ -1,15 +1,8 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { data } from '../../data';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class CharacterPageGuard implements CanActivate {
-  constructor(private router: Router) {}
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-    const characterExists = data[0].characters.some(c => c.id === route.params['characterId']);
-    return characterExists || this.router.createUrlTree(['/sf4']);
-  }
-}
+export const canActivateCharacter: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree => {
+  const characterExists = data[0].characters.some(c => c.id === route.params['characterId']);
+  return characterExists || inject(Router).createUrlTree(['/sf4']);
+};
